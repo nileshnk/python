36c7729f7e2c0b695a21bbd83d9f97b27aba13b5
Nilesh Kumar <nile.nil2@gmail.com>
2022-02-17 12:29:09 +0530

    Nearest-greater-element

---
+++ ngl.py
@@ -0,0 +1,80 @@
+# Python program to print next greater element using stack
+
+
+
+def createStack():
+	stack = []
+	return stack
+
+
+def isEmpty(stack):
+	return len(stack) == 0
+
+
+def push(stack, x):
+	stack.append(x)
+
+
+def pop(stack):
+	if isEmpty(stack):
+		print("Error : stack underflow")
+	else:
+		return stack.pop()
+
+
+
+
+def printNGE(arr):
+	s = createStack()
+	element = 0
+	next = 0
+
+
+	push(s, arr[0])
+
+	for i in range(1, len(arr), 1):
+		next = arr[i]
+
+		if isEmpty(s) == False:
+
+
+			element = pop(s)
+
+			
+			while element < next:
+				print(str(element) + " -- " + str(next))
+				if isEmpty(s) == True:
+					break
+				element = pop(s)
+
+			
+			if element > next:
+				push(s, element)
+				
+		push(s, next)
+
+	while isEmpty(s) == False:
+		element = pop(s)
+		next = -1
+		print(str(element) + " -- " + str(next))
+
+
+arr = [11, 13, 21, 3]
+
+val = []
+n = int(input("Enter the number of elements"))
+
+for i in range(0, n):
+	ele = int(input())
+	val.append(ele)
+
+
+#print(val)
+#for i in range(0, n):
+#	print(val[i]+"\n")
+
+
+printNGE(val)
+
+
+
