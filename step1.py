6a2c040c8a04d90a8986c3a322597bd5a172774b
Nilesh Kumar <nile.nil2@gmail.com>
2022-02-18 15:04:22 +0530

    link

--- step1.py
+++ step1.py
@@ -9,3 +9,5 @@ getPage.raise_for_status()
 menu = bs4.BeautifulSoup(getPage.text, 'html.parser')
 foods = menu.select('.gb1')
 print(foods)
+
+#https://www.codementor.io/@gergelykovcs/how-and-why-i-built-a-simple-web-scrapig-script-to-notify-us-about-our-favourite-food-fcrhuhn45#the-process-of-building-the-web-scrapig-script
